1bee42ca11c41bb0f7c933662fb99c70f6dbf524
luigidechiara <luigidechiara00@gmail.com>
2023-02-08 09:18:12 +0100

    test di unità.

--- EdilCommerce_Design/src/test/java/model/TestArticoloModelDS.java
+++ EdilCommerce_Design/src/test/java/model/TestArticoloModelDS.java
@@ -4,6 +4,8 @@ package model;
 import static org.junit.Assert.assertEquals;
 
 import java.sql.SQLException;
+import java.util.Collection;
+import java.util.LinkedList;
 
 import javax.sql.DataSource;
 
@@ -25,6 +27,7 @@ public class TestArticoloModelDS {
 	private ArticoloModelDS model;
 
 	private ArticoloBean articolo;
+	private ArticoloBean articolo2;
 	
 	@Before
 	public void setUp() throws SQLException, Exception {
@@ -46,8 +49,20 @@ public class TestArticoloModelDS {
 		articolo.setMediaRecensioni(0);
 		articolo.setNome("poltrona");
 		articolo.setNomeCategoria("arredamento");
-		 
+		
 		 model.doSave(articolo);
+		
+		articolo2=new ArticoloBean();
+		articolo2.setCodiceArticolo("ART01");
+		articolo2.setCosto(90.2);
+		articolo2.setDescrizione("ciao");
+		articolo2.setGiacenza(20);
+		articolo2.setImmagine("prova");
+		articolo2.setMediaRecensioni(0);
+		articolo2.setNome("tegola");
+		articolo2.setNomeCategoria("aEsterno");
+		 
+		 model.doSave(articolo2);
 		 
 		 
 		 
@@ -57,7 +72,7 @@ public class TestArticoloModelDS {
 
 	
     @Test
-	public void doRetriveByKeyTest() throws SQLException {
+	public void testDoRetriveByKey() throws SQLException {
     	ArticoloBean pre= new ArticoloBean();
     	pre=new ArticoloBean();
 		pre.setCodiceArticolo("ART00");
@@ -79,7 +94,71 @@ public class TestArticoloModelDS {
     	
     	}
 	
-	
+    @Test
+    public void testDoRetriveAll() {
+    	ArticoloBean a1= new ArticoloBean();
+      	a1=new ArticoloBean();
+  		a1.setCodiceArticolo("ART10");
+  		a1.setCosto(80.2);
+  		a1.setDescrizione("ciao");
+  		a1.setGiacenza(20);
+  		a1.setImmagine("prova");
+  		a1.setMediaRecensioni(0);
+  		a1.setNome("poltrona");
+  		a1.setNomeCategoria("arredamento"); 
+  		
+  		ArticoloBean a2= new ArticoloBean();
+      	a2=new ArticoloBean();
+  		a2.setCodiceArticolo("ART01");
+  		a2.setCosto(90.2);
+  		a2.setDescrizione("ciao");
+  		a2.setGiacenza(20);
+  		a2.setImmagine("prova");
+  		a2.setMediaRecensioni(0);
+  		a2.setNome("tegola");
+  		a2.setNomeCategoria("aEsterno"); 
+  		
+  		Collection<ArticoloBean> expected = new LinkedList<ArticoloBean>();
+  		expected.add(a1);
+  		expected.add(a2);
+  		
+  		Collection<ArticoloBean> actual = new LinkedList<ArticoloBean>();
+  		
+  		try {
+
+			actual = model.doRetriveAll("nome");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		}
+		assertEquals(expected, actual);
+  		
+    }
+    
+    @Test
+  	public void testDoSaveTest() throws SQLException {
+      	ArticoloBean pre= new ArticoloBean();
+      	pre=new ArticoloBean();
+  		pre.setCodiceArticolo("ART10");
+  		pre.setCosto(80.2);
+  		pre.setDescrizione("ciao");
+  		pre.setGiacenza(20);
+  		pre.setImmagine("prova");
+  		pre.setMediaRecensioni(0);
+  		pre.setNome("poltrona");
+  		pre.setNomeCategoria("arredamento");    	
+  		boolean confronto=false;
+      	try {
+      		confronto=model.doSave(pre);
+      		
+      	}catch(SQLException e) {
+      		e.printStackTrace();
+      	}
+      	assertEquals(true,confronto);
+      	
+      	}
+  	
 	
 	
 	
